0e5456a6d52420ac3758bcf084f9444cf83d6e0c
BANSHIDHARTech <banshidhar2005@gmail.com>
2025-09-15 15:19:23 +0530

    Delete supabase directory

--- supabase/migrations/20240914_fix_agents_system_prompt.sql
+++
@@ -1,9 +0,0 @@
--- Add default value for system_prompt in agents table
-ALTER TABLE IF EXISTS public.agents 
-    ALTER COLUMN system_prompt SET NOT NULL,
-    ALTER COLUMN system_prompt SET DEFAULT '';
-
--- Update any existing NULL values to empty string
-UPDATE public.agents 
-SET system_prompt = '' 
-WHERE system_prompt IS NULL;
